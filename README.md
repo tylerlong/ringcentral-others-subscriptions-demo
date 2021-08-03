cc58fa2856e1506752b44f88a5acd273b186ec8d
Tyler Long <tyler4long@gmail.com>
2021-08-03 15:49:13 -0700

    First working version

---
+++ README.md
@@ -0,0 +1,3 @@
+# ringcentral-others-subscriptions-demo
+
+As I tested, one cannot see or manage others' subscriptions, even if he is super admin.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import PubNubExtension from '@rc-ex/pubnub';
+
+const rc1 = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const rc2 = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+(async () => {
+  await rc1.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+
+  await rc2.authorize({
+    username: process.env.RINGCENTRAL_USERNAME_2!,
+    extension: process.env.RINGCENTRAL_EXTENSION_2,
+    password: process.env.RINGCENTRAL_PASSWORD_2!,
+  });
+  // const pubnubExtension = new PubNubExtension();
+  // await rc2.installExtension(pubnubExtension);
+  // pubnubExtension.subscribe(['/restapi/v1.0/account/~/extension'], () => {});
+
+  const subs1 = await rc1.restapi().subscription().list();
+  const subs2 = await rc2.restapi().subscription().list();
+
+  console.log(JSON.stringify(subs1, null, 2));
+  console.log(JSON.stringify(subs2, null, 2));
+
+  const sub1 = await rc1.restapi().subscription(subs2.records![0].id).get();
+  console.log(JSON.stringify(sub1, null, 2));
+
+  // const sub2 = await rc2.restapi().subscription(subs2.records![0].id).get();
+  // console.log(JSON.stringify(sub2, null, 2));
+
+  await rc1.revoke();
+  await rc2.revoke();
+})();
